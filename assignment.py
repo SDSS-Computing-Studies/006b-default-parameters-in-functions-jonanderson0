6eeebc21635d4123f341a188644370dda7bcdf6f
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-13 12:24:44 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,7 +2,13 @@
 
 import math
 
-def tempConversion():
+def tempConversion(degrees, unit="C"):
+  deg1=(degrees*1.8)+32
+ if unit=="F":
+  deg1=((degrees-32)*(5/9))
+  deg1=round(deg1,1)
+  return deg1
+
 
 
 def factorPair():
