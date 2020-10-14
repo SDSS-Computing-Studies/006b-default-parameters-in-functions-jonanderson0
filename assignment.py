4c6b652654edbc38b4a75cbf71f935e4cabdca12
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-13 22:53:19 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -44,6 +44,10 @@ def solution(solution):
   x2 = solutions[1]
   return x2
 
-def quadratic():
-  pass
+def quadratic(a,b,c):
+  x1 = (-b + math.sqrt((b ** 2) - 4 * a * c)) / (2 * a)
+  x2 = (-b - math.sqrt((b ** 2) - 4 * a * c)) / (2 * a)
+  quadAnswer = [x1,x2]
+  quadAnswer.sort()
+  return quadAnswer
 
